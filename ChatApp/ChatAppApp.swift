27b26fb7263fe780b1edc506568f43890e096368
Jakir Hossain <jakirseu@gmail.com>
2023-10-05 15:43:17 +0600

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Jakir Hossain on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
